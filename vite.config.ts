2c496ac9b0597949d4eb70d748ceb344bad24639
FirstMateFir <elya9357@gmail.com>
2026-02-18 13:59:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/iliaKuznetsov-portfolio/" : "/",
+  base: mode === "production" ? "/iliakuznetsov-portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
